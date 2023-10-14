44a15f2227b5e47c42ed9b51218a33d9de04e77b
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2023-10-14 15:04:09 +0530

    Create Display the duplicate elements in an array.cpp

---
+++ Display the duplicate elements in an array.cpp
@@ -0,0 +1,42 @@
+
+//Display the duplicate elements in an array
+
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[100];
+    int n;
+    int i, j;
+        cout<<"Input the number of elements to be stored in the array :";
+        cin>>n;
+        cout<<"Input "<<n<<" elements in the array :\n";
+        for(i=0;i<n;i++)
+        {
+            cout<<"element - "<<i<<" : ";
+            cin>>arr[i];
+        }
+        for (i = 0; i < n; i++) {
+            for (j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    
+                    cout<<"\nThe duplicate element is : "<<arr[i];
+                    break;
+                }
+            }
+        }
+    
+    return 0;
+}
+
+/*  Output
+
+Input the number of elements to be stored in the array :5
+Input 5 elements in the array :
+element - 0 : 1
+element - 1 : 1
+element - 2 : 13
+element - 3 : 4
+element - 4 : 4
+
+The duplicate element is : 1
+The duplicate element is : 4  */
