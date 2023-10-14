7332daaa9778d4210e7ea5b647605d987a2acd6d
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2023-10-14 12:40:03 +0530

    Update reversal of array.c

--- reversal of array.c
+++ reversal of array.c
@@ -1,21 +1,21 @@
 
 // Write a C program to read n number of values in an array and display them in reverse order.
 
-#include<stdio.h>
+#include<stdio.h>   //preprocessor diredtive
 void main(){
-    int arr[100],n;
+    int arr[100],n;   //Initialization 
     printf("Enter the number of elements to be stored : ");
     scanf("%d",&n);
-    printf("Enter %d elements : ",n);
+    printf("Enter %d elements : ",n);   //Getting the input of array based on the n 
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    printf("\nThe original array:");
+    printf("\nThe original array:");   //printing the original array
     for(int i=0;i<n;i++){
         printf("\nArray[%d] is %d",i,arr[i]);
     }
     printf("\n");
-    printf("\nThe reversed array:");
+    printf("\nThe reversed array:");   //Printing the reversed array 
     for(int i=n-1;i>=0;i--){
         printf("\nArray[%d] is %d",i,arr[i]);
     }
