aa4b9537d3a2b092c04f7d64bb3efd5a8f680c01
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2023-10-14 12:20:03 +0530

    Create reversal of array.c

---
+++ reversal of array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main(){
+    int arr[50],n;
+    printf("Enter the number of elements to be stored : ");
+    scanf("%d",&n);
+    printf("Enter %d elements : ",n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("\nThe original array:");
+    for(int i=0;i<n;i++){
+        printf("\nArray[%d] is %d",i,arr[i]);
+    }
+    printf("\n");
+    printf("\nThe reversed array:");
+    for(int i=n-1;i>=0;i--){
+        printf("\nArray[%d] is %d",i,arr[i]);
+    }
+    
+}
